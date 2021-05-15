a953a7bee73bd0b4cc96eb0e24f11f7763e0f36f
kirkor <43179639+kirkorz@users.noreply.github.com>
2021-05-15 11:03:48 +0700

    Update answerdb.js

--- mongodbquery/answerdb.js
+++ mongodbquery/answerdb.js
@@ -5,16 +5,47 @@ var ObjectID = require('mongodb').ObjectID;
 
 let getAnswers = async(data)=>{
     try{
+        var skip = data.skip;
+        var limit = data.limit;
         const client = new MongoClient(uri, { useUnifiedTopology: true } );
         await client.connect({native_parser:true});
-        const result = await client.db("ptud-15").collection("comments").findOne({
-            'node_id':ObjectID(data.node_id),
-            'page': data.page * 1
-        });
-        const count = await client.db("ptud-15").collection("questions").findOne({'_id':ObjectID(data.node_id)},{ projection:{'page_of_comment':1}});
-        await client.close();
-        console.log(result,count);
-        return {result,count};
+        var query = await client.db("ptud-15").collection("comments").find({
+            'node_id':ObjectID(data.node_id)
+        })
+        var result = [];
+        query = await query.sort({"page":1}).toArray();
+        for(let page of query){
+            new_skip = skip - page['count']
+            if(new_skip >= 0){
+                skip =  new_skip;
+                continue;
+            }
+            else if (skip > 0){
+                comments = page['comments'].slice(skip,page['count']);
+            }
+            else{
+                comments = page['comments'];
+            }
+            skip = new_skip;
+            for(let cmt of comments){
+                if(limit == 0){
+                    break;
+                }
+                limit -= 1;
+                result.push(cmt)
+            }
+            if(limit==0){
+                break;
+            }
+        }
+        // const result = await client.db("ptud-15").collection("comments").findOne({
+        //     'node_id':ObjectID(data.node_id),
+        //     'page': data.page * 1
+        // });
+        // const count = await client.db("ptud-15").collection("questions").findOne({'_id':ObjectID(data.node_id)},{ projection:{'page_of_comment':1}});
+        // await client.close();
+        // console.log(result);
+        return {result,skip: data.skip,limit: data.limit};
     } catch(err){
         throw err;
     }   

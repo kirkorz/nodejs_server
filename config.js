8e01f40a66631e76d4b58843fbd921a150d81f1e
kirkor <43179639+kirkorz@users.noreply.github.com>
2021-05-19 11:46:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 var config ={}
-// config.mongodb = 'mongodb+srv://baor000:moFexFUmwovUMN2Q@cluster0.e3ane.gcp.mongodb.net/ptud-15?retryWrites=true&w=majority';
-config.mongodb = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false';
+config.mongodb = 'mongodb+srv://baor000:moFexFUmwovUMN2Q@cluster0.e3ane.gcp.mongodb.net/ptud-15?retryWrites=true&w=majority';
+// config.mongodb = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false';
 module.exports = config;
